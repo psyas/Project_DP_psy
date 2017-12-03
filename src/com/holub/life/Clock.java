5f16c5d3b9428029eed3f4a5c1ba0d23944082be
JiYeon <kk9405@naver.com>
2017-12-03 21:39:38 +0900

    singleton pattern - jiyeon

--- src/com/holub/life/Clock.java
+++ src/com/holub/life/Clock.java
@@ -38,15 +38,20 @@ public class Clock
 	{	createMenus();
 	}
 
-	private static Clock instance;
+	private volatile static Clock instance;
 
 	/** The clock is a singleton. Get a reference to it by calling
 	 *  <code>Clock.instance()</code>. It's illegal to call
 	 *  <code>new Clock()</code>.
 	 */
-	public synchronized static Clock instance()
-	{	if( instance == null )
-			instance = new Clock();
+
+	public static Clock instance()
+	{	if( instance == null ){
+		synchronized (Clock.class){
+			if(instance == null)
+				instance = new Clock();
+		}
+	}
 		return instance;
 	}
 
